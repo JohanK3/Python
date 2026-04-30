b9c29c7c44ed030d96e9e91f130253e6dc809255
Johan Karl <johankarlkassa@gmail.com>
2026-04-30 19:35:15 +0100

    Correction

--- Exercice1.py
+++ Exercice1.py
@@ -7,13 +7,13 @@ while True :
         # PrixTTC=X*(1+TVA)    .15 = 0.15 (15%)
 
         print("Le prix TTC de notre prix HT : {0:.2f} est : {1:.2f}".format(PrixHT, PrixHT * (1 + .15)))
-        # {:.2f} c'est pour dire que je veux 2 chiffre après la virgurle
+        # {:.2f} c'est pour dire que je veux 2 chiffre après la virgurle.
         break
     except ValueError :
         print("Faites attention Erreur de saisie :( ")
 
 
-# float parce que enventuelement il y a des , flottante dans les prix come 8.990dt
+# float parce que éventuelement il y a des , flottante dans les prix comme 8.990dt.
 
 
 
