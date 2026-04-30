eee074216ad14f64c4e1465eab92aa6003c867b4
Johan Karl <johankarlkassa@gmail.com>
2026-04-30 19:46:43 +0100

    test pylint

--- Exercice1.py
+++ Exercice1.py
@@ -1,20 +1,17 @@
 # Récupérer le un prix HT et afficher le PrixTTC
+# PrixTTC=X*(1+TVA)    .15 = 0.15 (15%)
 
 while True :
     try :
         PrixHT = float(input("Donnez votre prix HT : "))
+        # float parce que éventuelement il y a des , flottante dans les prix comme 8.990dt.
 
-        # PrixTTC=X*(1+TVA)    .15 = 0.15 (15%)
-
-        print("Le prix TTC de notre prix HT : {0:.2f} est : {1:.2f}".format(PrixHT, PrixHT * (1 + .15)))
+        print("Le prix TTC de notre prix HT : {0:.2f} "
+            "est : {1:.2f}".format(PrixHT, PrixHT * (1 + .15)))
         # {:.2f} c'est pour dire que je veux 2 chiffre après la virgurle.
         break
     except ValueError :
         print("Faites attention Erreur de saisie :( ")
-
-
-# float parce que éventuelement il y a des , flottante dans les prix comme 8.990dt.
-
 
 
 
